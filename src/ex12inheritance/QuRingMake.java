924e8303f3376f145e5c8544f20e3fa787c2a58e
hongicsam1514 <hongicsam1514@gmail.com>
2023-07-12 18:07:35 +0900

    2023년07월12일

--- src/ex12inheritance/QuRingMake.java
+++ src/ex12inheritance/QuRingMake.java
@@ -6,11 +6,14 @@ Point 클래스를 기반으로 원을 의미하는 Circle클래스를 정의하
 그리고 Circle클래스를 기반으로 Ring클래스를 정의하자.
  */
 class Point {
+	// 멤버변수 : 중심점 표현을 위해 x, y좌표값을 설정
    	int xDot, yDot;
+   	// 생성자
    	public Point(int x, int y) {
  		xDot=x;
  		yDot=y;
    	}
+   	// 멤버변수인 좌표값을 출력
    	public void showPointInfo() {
  		System.out.println("[x좌표:"+xDot+", y좌표:"+yDot+"]");
    	}
@@ -20,9 +23,19 @@ class Circle{
 	//멤버변수
 	int radian;//반지름
 	Point center;
-	public Circle(int x, int y, int radian) {
-		this.center = new Point(x, y);
+	// 생성자
+	public Circle(int radian, int x, int y) {
+		// 멤버변수 반지름을 초기화
 		this.radian = radian;
+		// Point인스턴스를 생성 및 초기화
+		this.center = new Point(x, y);
+	}
+	// 원의 정보를 출력하는 멤버메서드
+	public void showCircleInfo() {
+		// 포인트 정보를 먼저 출력한다.
+		center.showPointInfo();
+		//반지름을 출력한다.
+		System.out.println("반지름 : " + this.radian);
 	}
 }
 //원 2개를 겹쳐서 링을 표현하는 클래스
@@ -30,24 +43,34 @@ class Ring{
 	//멤버변수
 	Circle innerCircle;//안쪽의 원
 	Circle outerCircle;//바깥쪽의 원
+	// 생성자
 	public Ring(int inX, int inY, int inRadian, int outX, int outY, int outRadian) {
-		this.innerCircle = new Circle(inX, inY, inRadian);
-		this.outerCircle = new Circle(outX, outY, outRadian);
+		// 안쪽 원 인스턴스 생성
+		this.innerCircle = new Circle(inRadian, inX, inY);
+		// 바깥쪽 원 인스턴스 생성
+		this.outerCircle = new Circle(outRadian, outX, outY);
 	}
+	// 링의 정보를 출력하는 멤버메서드
 	public void showRingInfo() {
+		// 링은 원 2개의 정보를 동시에 출력하면된다.
 		System.out.println("안쪽원의 정보 : ");
-		System.out.println("반지름 : " + innerCircle.radian);
-		innerCircle.center.showPointInfo();
-		
+		innerCircle.showCircleInfo();
 		System.out.println("바깥쪽원의 정보 : ");
-		System.out.println("반지름 : " + outerCircle.radian);
-		outerCircle.center.showPointInfo();
+		outerCircle.showCircleInfo();
 	}
 	
 }
 public class QuRingMake {
 
 	public static void main(String[] args) {
+		/*
+		안쪽원의 정보 :
+		반지름 : 3	
+		[x좌표:1, y좌표1]
+		바깥쪽원의 정보 :
+		반지름 : 9
+		[x좌표:2, y좌표2]
+		 */
 		Ring c = new Ring(1,1,3,2,2,9);
 		c.showRingInfo();
 	}
